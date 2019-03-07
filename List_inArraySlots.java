80e29aabd877d65e333f7930d74d1c98831b6297
Faria Huq <46835579+fariahuq@users.noreply.github.com>
2019-03-06 22:15:30 -0500

    Add files via upload

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -5,50 +5,62 @@
 
 public class List_inArraySlots {
 
-    // declare fields here
+    public int[] ArrayList;
+    public int filledElements;
+    public int capacity;
 
     /**
       Construct an empty list with a small initial capacity.
      */
     public List_inArraySlots() {
+      capacity = 10;
+      ArrayList = new int[capacity];
     }
 
 
-    /** 
+    /**
       @return the number of elements in this list
      */
-    // public int size() {
-    // }
+    public int size() {
+      return filledElements;
+
+    }
 
 
-     /** 
+     /**
        @return a string representation of this list,
        in [a,b,c,] format
-      */ 
-    // public String toString() {
-    // }
+      */
+    public String toString() {
+      String result = "[";
+      for (int i = 0; i < filledElements; i++)
+        return result += ArrayList[i] + ", ";
+      return result + "]";
+}
 
-    
-    /** 
-      Appends @value to the end of this list.
-      
-      @return true, in keeping with conventions yet to be discussed
-     */
-     // public boolean add( int value) {
-     // }
+    public boolean add( int value) {
+      if (filledElements == ArrayList.length)
+        expand();
+      ArrayList[filledElements] = value;
+      filledElements++;
+      return true;
+
+  }
 
 
-    /** 
-      Double the capacity of the List_inArraySlots, 
+    /**
+      Double the capacity of the List_inArraySlots,
       preserving existing data
      */
-     // private void expand() {
-        // System.out.println( "expand... (for debugging)");
+     private void expand() {
+        System.out.println( "expand... (for debugging)");
            // /* S.O.P. rules for debugging:
-              // Working methods should be silent. But during 
-              // development, the programmer must verify that 
+              // Working methods should be silent. But during
+              // development, the programmer must verify that
               // this method is called when that is appropriate.
               // So test using the println(), then comment it out.
               // */
-     // }
+      }
+
+
 }
